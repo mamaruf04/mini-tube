a9698c3b59a17d868d1c7082e936bc225c4a9fca
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-03-08 13:01:45 +0600

    router managed by react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
-// import Video from "./pages/Video";
+import Video from "./pages/Video";
 
 function App() {
-    return <Home />
+  return (
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path = "/" element={<Home></Home>}></Route>
+        <Route path = "/video/:videoId" element={<Video></Video>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
